2bfa5741ecdc4524aeb60d9c48b52347e5e15c38
Muhammad Bin Yasir <mbyasir5@gmail.com>
2024-08-16 17:01:55 +0500

    Your commit message here

--- components/board.tsx
+++ components/board.tsx
@@ -1,6 +1,19 @@
 "use client"
-import React, { useRef, useState } from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import AlertDialogs from './alert';
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from './ui/button';
+import { truncateText } from '@/lib/utils';
 
 const Board = () => {
     const [isDialogOpen, setDialogOpen] = useState<boolean>(false);
@@ -14,6 +27,9 @@ const Board = () => {
     const [x, setX] = useState(0);
     const [O, setO] = useState(0);
     const [win, setWin] = useState<string>("")
+
+    const [player1, setPlayer1] = useState("Player 1")
+    const [player2, setPlayer2] = useState("Player 2")
 
     const handleClick = (key: number) => {
         if (lock || data[key]) {
@@ -42,7 +58,7 @@ const Board = () => {
             setWin(winner)
             openDialog();
         } else if (newData.every(cell => cell)) {
-            setLock(true); 
+            setLock(true);
             setWin("draw")
             openDialog();
         }
@@ -80,17 +96,34 @@ const Board = () => {
         setO(0)
     }
 
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = e.target;
+
+        if (name === "player1") {
+            setPlayer1(value);
+        } else if (name === "player2") {
+            setPlayer2(value);
+        }
+
+    }
+
     return (
         <>
             <h4 className="text-white font-bold text-2xl mb-3 text-center">TicTacToe Game</h4>
             <div className="grid grid-cols-2 gap-2 my-3">
-                <div className='bg-white w-full rounded flex text-slate-900 gap-4 font-bold text-lg px-3 py-4'>
+                <div className='bg-white w-full rounded flex items-center text-slate-900 gap-4 px-3 py-4'>
                     <img src='/cross.svg' className='w-8 h-8' />
-                    {x}
+                    <div>
+                        <h4 className='font-bold text-lg'> {truncateText(player1, 5)} </h4>
+                        <p className="text-sm">{x}</p>
+                    </div>
                 </div>
-                <div className='bg-white w-full rounded flex text-slate-900 gap-4 font-bold text-lg px-3 py-4'>
+                <div className='bg-white w-full rounded flex items-center text-slate-900 gap-4 px-3 py-4'>
                     <img src='/o.svg' className='w-8 h-8' />
-                    {O}
+                    <div>
+                        <h4 className='font-bold text-lg'> {truncateText(player2, 5)} </h4>
+                        <p className="text-sm">{O}</p>
+                    </div>
                 </div>
             </div>
             <div className="grid grid-cols-3 gap-3">
@@ -105,10 +138,42 @@ const Board = () => {
                 ))}
             </div>
             <div className="mx-auto mt-3">
-                
+
                 <button className='w-full h-10 bg-red-100 font-bold text-red-900 rounded' onClick={resetGame}>Reset Game</button>
             </div>
             <AlertDialogs open={isDialogOpen} onClose={closeDialog} winner={win} onClick={resetBoard} />
+            <AlertDialog defaultOpen={true}>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle className='flex items-center gap-2'>Give Name of Players Before We Continue.</AlertDialogTitle>
+                    </AlertDialogHeader>
+                    <AlertDialogDescription>Give The Name of Players To Continue..</AlertDialogDescription>
+                    <div className="mt-5">
+                        <div className="flex flex-col gap-3">
+                            <div className="rounded flex w-full h-14 items-center gap-2 border border-slate-100">
+                                <div className="p-4 w-14 h-14"><img src='/cross.svg' /></div>
+                                <input
+                                    className='w-full h-full border-none outline-none '
+                                    name="player1"
+                                    value={player1}
+                                    onChange={(e) => handleInput(e)} />
+                            </div>
+                            <div className="rounded flex w-full h-14 items-center gap-2 border border-slate-100">
+                                <div className="p-4 w-14 h-14"><img src='/o.svg' /></div>
+                                <input
+                                    className='w-full h-full border-none outline-none '
+                                    name="player2"
+                                    value={player2}
+                                    onChange={(e) => handleInput(e)} />
+                            </div>
+                        </div>
+                    </div>
+                    <AlertDialogFooter>
+                        <AlertDialogAction>Submit</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+
         </>
     )
 }
